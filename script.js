28be579bd6454d992b4532da609f6fb2f1304175
marta vasallo <mvasalloperez@gmail.com>
2023-09-21 16:01:45 +0100

    added sorting method

--- script.js
+++ script.js
@@ -68,11 +68,19 @@ const currencies = new Map([
 
 
 // ----------- DISPLAY MOVEMENTS DEPOSIT AND WITHDREW --------------------------
-const displayMovements = function (movements) {
+// --- SORT movements ( sort = false) - added at the end ----
+
+const displayMovements = function (movements, sort = false) {
   // take out default movements
   containerMovements.innerHTML = "";
 
-  movements.forEach(function (mov, i) {
+  // --- SORTING MOVEMENTS
+  // use .slice to make a copy or the array
+  const movs = sort ? movements.slice().sort((a,b) => a - b) : movements
+  // calling function right at the end
+
+  // before sorting method, movs = movements
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const html = `
@@ -231,6 +239,27 @@ btnTransfer.addEventListener('click', function(e) {
   }
 })
 
+
+//------------------------ LOAN ------------------------------------------------
+// bank only grants a loan if there is at least one deposit with at least 10% of requested loan amount
+btnLoan.addEventListener('click', function(e) {
+  e.preventDefault()
+
+  const amount = Number(inputLoanAmount.value)
+
+  // if at least one of the elements is 10% higher than a deposit
+  if (amount > 0 && currentAccount.movements.some(mov =>
+    mov >= amount * 0.1)) {
+    // add movement
+    currentAccount.movements.push(amount)
+
+    // update UI
+    updateUI(currentAccount)
+  }
+  inputLoanAmount.value = ''
+  inputLoanAmount.blur()
+})
+
 // -------------------- CLOSE ACCOUNT ----------------------------------------
 // deletes de object from the accounts array
 btnClose.addEventListener('click', function (e) {
@@ -257,7 +286,19 @@ btnClose.addEventListener('click', function (e) {
 
 })
 
+// to click again to keep sorting it
+let sorted = false;
+//add event to button
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault()
+  displayMovements(currentAccount.movements, !sorted)
+  sorted = !sorted;
+})
 
+
+
+/////////////////////////////// LECTURE ////////////////////////////////////////
+const movements2 = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // -------------------- WORKING BALANCE ----------------------------------------
 
@@ -265,7 +306,6 @@ btnClose.addEventListener('click', function (e) {
 // to filter for elements that satisfy a certain condition
 // we want to create an array od the deposits (movements > 0)
 // only the values that pass that condition (true) will be in the new array
-const movements2 = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const deposits = movements2.filter(function (mov) {
   return mov > 0;
@@ -311,7 +351,7 @@ const totalDepositToUsd = movements2
 // console.log(totalDepositToUsd);
 
 
-//--------------------   JUST PRACTICE FIND METHOD  ----------------------------
+//--------------------  FIND METHOD  -------------------------------------------
 // FIND METHOD - does not return a new array, just the element itself
 // to retrieve one element of an array based o a condition
 // accepts a condition
@@ -327,3 +367,96 @@ const firstWithdrawal = movements2.find(function (mov) {
 // can also find objects
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 // console.log(account); // = {owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222, username: 'jd'}
+
+
+//--------------------   SOME METHOD  ------------------------------------------
+// .includes only tests for equality
+console.log(movements2.includes(-130)); // true
+
+// .some tests for a condition
+// returns a boolean
+// check if there is any positive movements in the array
+const anyDeposits = movements2.some(mov => mov > 0)
+
+//--------------------   EVERY METHOD  -----------------------------------------
+// .every returns true if all the elements in the array satisfy the condition
+
+console.log(movements2.every(mov => mov > 0));  // false
+
+
+//--------------------   new FLAT METHOD  --------------------------------------
+// takes all elements from an array and puts them all together in one big array
+// only workd one level deep [array [array]]
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8]
+console.log(arr.flat()); // = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// add how many levels you want to go deep
+const arrDeep = [[1, 2, 3], [4,[5, 6]], 7, 8]
+console.log(arrDeep.flat(2)); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+//--------------------   new MAP METHOD  --------------------------------------
+
+const accountMovements = accounts.map(acc => acc.movements)
+console.log(accountMovements);
+/*
+[200, 450, -400, 3000, -650, -130, 70, 1300]
+[5000, 3400, -150, -790, -3210, -1000, 8500, -30]
+[200, -200, 340, -300, -20, 50, 400, -460]
+[430, 1000, 700, 50, 90]
+*/
+const allMovements = accountMovements.flat()
+console.log(allMovements); // [200, 450, -400, 3000, -650, -130, 70, 1300, 5000, 3400, -150, -790, -3210, -1000, 8500, -30, 200, -200, 340, -300, -20, 50, 400, -460, 430, 1000, 700, 50, 90]
+const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0)
+console.log(overallBalance); // 17840
+
+// chaining all this methods:
+
+// const overallBalance = accounts
+//   .map(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0)
+// console.log(overallBalance);
+
+//--------------------   FLATMAP METHOD --------------------------------------
+// combines new flat and new map methos
+
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0)
+console.log(overallBalance2);
+
+
+//-------------------- SORTING OUT ARRAYS --------------------------------------
+// .SORT - mutates originall array
+// by default only works with strings
+// use when array ONLY numbers
+
+const owners = ['Marta', 'Eugenia', 'Ana', 'Carlos']
+console.log(owners.sort()); // ['Ana', 'Carlos', 'Eugenia', 'Marta']
+console.log(owners); // MUTATED ['Ana', 'Carlos', 'Eugenia', 'Marta']
+
+
+// .sort with numbers:
+// give a callback function
+// array.sort((a, b) => {})
+// array.sort((current_value, next_value) => {})
+// return < 0  then  A will be sorted before B (keep order)
+// return > 0  then  B, A (switch order)
+
+// ascending order
+movements2.sort((a,b) => a - b) // [-650, -400, -130, 70, 200, 450, 1300, 3000]
+// same as
+movements2.sort((a,b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+})
+console.log(movements2);
+
+// descending order
+movements2.sort((a,b) => b - a) // [3000, 1300, 450, 200, 70, -130, -400, -650]
+// same as
+movements2.sort((a,b) => {
+  if (a > b) return -1;
+  if (a < b) return 1;
+})
+console.log(movements2);
